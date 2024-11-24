093278298618113f8ef92c6791e34012cb142cc1
Francis <francism4402@gmail.com>
2024-11-24 13:44:34 +0600

    build file update complete assignment

--- dist/app.js
+++ dist/app.js
@@ -13,6 +13,11 @@ app.use(express_1.default.json());
 app.use('/api/products', bookShop_routes_1.BookShopRoutes);
 app.use('/api/orders', bookShopOrder_routes_1.OrderRoutes);
 app.get('/', (req, res) => {
-    res.send({ Title: 'Programming Hero Next Level Assignment 2 (Book-Shop)', Name: 'Prithvy Francis Manda', Batch: '4', StudentID: 'WEB8-1601' });
+    res.send({
+        Title: 'Programming Hero Next Level Assignment 2 (Book-Shop)',
+        Name: 'Prithvy Francis Manda',
+        Batch: '4',
+        StudentID: 'WEB8-1601',
+    });
 });
 exports.default = app;
